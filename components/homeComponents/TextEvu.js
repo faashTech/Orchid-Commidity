7f2e7f768aa6d541b842050ddf13efd92ab9273a
faashTech <157911836+faashTech@users.noreply.github.com>
2024-09-11 15:33:12 +0300

    Add files via upload

--- components/homeComponents/TextEvu.js
+++ components/homeComponents/TextEvu.js
@@ -6,14 +6,13 @@ const data = [
     "Kamoa-Kakula Copper Complex",
     "Western Foreland Copper Exploration",
     "Kipushi Zinc Mine",
-    "Platreef PGM-Nickel Mine",
-    "Regional Exploration",
+    "Kamao copper mine zinc",
 ]
 
 
 function TextEvu() {
   return (
-    <div className='container mx-auto p-6 md:flex flex-wrap gap-3 justify-between hidden my-20 text-white py-20 '>
+    <div className='container mx-auto p-6 md:flex  gap-3 justify-between hidden my-20 text-white py-20 '>
             {data.map(title=>{
                 return <h1 key={randomID} className='font-bold cursor-pointer text-3xl hover:text-orange-400 border-r-2 border-white px-3'>{title}</h1>
             })}
